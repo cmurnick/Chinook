32433a166650c9dd0d47388b5c4615d9c8236e9d
Caitlin Murnick <caitlinmurnick@gmail.com>
2018-02-19 18:45:55 -0600

    brazil_customers_invoices.sql

--- Chinook.sql
+++ Chinook.sql
@@ -5,3 +5,11 @@ WHERE Country <> 'US';
 SELECT FirstName, LastName, CustomerId
 FROM Customer
 WHERE Country = 'Brazil';
+
+SELECT Customer.FirstName + ' '+ Customer.LastName Customer, Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry
+FROM Customer
+Inner JOIN Invoice ON Invoice.CustomerId=Customer.CustomerId
+ WHERE Customer.Country = 'Brazil'
+ 
+
+
